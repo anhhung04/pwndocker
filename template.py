3d0cbc3610f1ad0031a15e8af0b86796759b08b1
Anh-Hung Hoang <86340766+anhhung04@users.noreply.github.com>
2024-06-08 23:23:30 +0700

    Update template.py

--- template.py
+++ template.py
@@ -1,33 +1,30 @@
 #!/usr/bin/env python3
 from pwn import *
-import sys
-
 {bindings}
-
-REMOTE = ("localhost", 1337)
-GDB_SCRIPT = """"""
-
 context.arch = {bin_name}.arch
 context.binary = {bin_name}
-
-
+context.log_level = 'debug' if (args.DEBUG or args.GDB) else 'info'
+def slog(n, m): return success(': '.join([n, hex(m)]))
+s = lambda x: r.send(x) if isinstance(x, bytes) else r.send(str(x).encode())
+sl = lambda x: r.sendline(x) if isinstance(x, bytes) else r.sendline(str(x).encode())
+sa = lambda a, x: r.sendafter(str(a), x) if isinstance(x, bytes) else r.sendafter(str(a), x)
+sla = lambda a, x: r.sendlineafter(str(a), x) if isinstance(x, bytes) else r.sendlineafter(str(a), x)
 def conn():
-    global {bin_name}
     if args.LOCAL or args.GDB:
         r = process({proc_args})
         if args.GDB:
             gdb.attach(target=r, gdbscript=GDB_SCRIPT)
-            context.terminal = ['tmux', 'splitw', '-h']
     else:
         r = remote(*REMOTE)
     return r
 
+REMOTE = ("localhost", 1337)
+GDB_SCRIPT = """
 
-if __name__ == "__main__":
-    if args.DEBUG or args.GDB:
-        context.log_level = 'debug'
-    r = conn()
+""".strip()
 
-    # pwn here
+r = conn()
 
-    r.interactive()
+
+
+r.interactive()
